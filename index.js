0753baffd6a5b48a3ea09dc6a5d3d6abc933ac4e
scottsc0de <scott5902@gmail.com>
2022-11-17 06:55:11 -0500

    added initial prompt questions

--- index.js
+++ index.js
@@ -16,6 +16,9 @@
 // WHEN I decide to finish building my team
 // THEN I exit the application, and the HTML is generated
 
+// npm init -y
+// npm i inquirer@8.2.4
+
 const inquirer = require('inquirer');
 const fs = require('fs');
 
@@ -40,4 +43,94 @@ inquirer
             type: 'input',
             message: 'Please enter your office number',
             name: 'manager_office_number',
+        },
+        {
+            type: 'list',
+            message: 'Add an engineer or intern to your page?',
+            choices: ['Engineer', 'Intern'],
+            name: 'employee_type',
+        },
+        {
+            type: "input",
+            name: "engineer_name",
+            message: "Please enter engineer's name",
+            when: (answers) => {
+                if (answers.employee_type === "Engineer") {
+                    return true;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "engineer_id",
+            message: "Please enter engineer's id",
+            when: (answers) => {
+                if (answers.employee_type === "Engineer") {
+                    return true;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "engineer_email",
+            message: "Please enter engineer's email",
+            when: (answers) => {
+                if (answers.employee_type === "Engineer") {
+                    return true;
+                }
+            }
+        }, {
+            type: "input",
+            name: "engineer_github",
+            message: "Please enter engineer's github",
+            when: (answers) => {
+                if (answers.employee_type === "Engineer") {
+                    return true;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "intern_name",
+            message: "Please enter intern's name",
+            when: (answers) => {
+                if (answers.employee_type === "Intern") {
+                    return true;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "intern_id",
+            message: "Please enter intern's id",
+            when: (answers) => {
+                if (answers.employee_type === "Intern") {
+                    return true;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "intern_email",
+            message: "Please enter intern's email",
+            when: (answers) => {
+                if (answers.employee_type === "Intern") {
+                    return true;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "intern_school",
+            message: "Please enter intern's school",
+            when: (answers) => {
+                if (answers.employee_type === "Intern") {
+                    return true;
+                }
+            }
+        },
+        {
+            type: "confirm",
+            name: "continue",
+            message: "Would you like to add another team member?",
         }]);
