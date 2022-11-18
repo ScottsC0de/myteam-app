de493dec95cb2d364a4ad74901f177cd7b1db50e
scottsc0de <scott5902@gmail.com>
2022-11-18 09:35:00 -0500

    linked index with Employee and setup prompt draft

--- index.js
+++ index.js
@@ -19,122 +19,130 @@
 // npm init -y
 // npm i inquirer@8.2.4
 
-const inquirer = require('inquirer');
-const fs = require('fs');
+const Employee = require('./lib/Employee');
 
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: "What is your team manager's name?",
-            name: 'manager_name',
-        },
-        {
-            type: 'input',
-            message: 'Please enter your employee ID?',
-            name: 'manager_id',
-        },
-        {
-            type: 'input',
-            message: 'Please enter your email address',
-            name: 'manager_email',
-        },
-        {
-            type: 'input',
-            message: 'Please enter your office number',
-            name: 'manager_office_number',
-        },
-        {
-            type: 'list',
-            message: 'Add an engineer or intern to your page?',
-            choices: ['Engineer', 'Intern'],
-            name: 'employee_type',
-        },
-        {
-            type: "input",
-            name: "engineer_name",
-            message: "Please enter engineer's name",
-            when: (answers) => {
-                if (answers.employee_type === "Engineer") {
-                    return true;
-                }
-            }
-        },
-        {
-            type: "input",
-            name: "engineer_id",
-            message: "Please enter engineer's id",
-            when: (answers) => {
-                if (answers.employee_type === "Engineer") {
-                    return true;
-                }
-            }
-        },
-        {
-            type: "input",
-            name: "engineer_email",
-            message: "Please enter engineer's email",
-            when: (answers) => {
-                if (answers.employee_type === "Engineer") {
-                    return true;
-                }
-            }
-        }, {
-            type: "input",
-            name: "engineer_github",
-            message: "Please enter engineer's github",
-            when: (answers) => {
-                if (answers.employee_type === "Engineer") {
-                    return true;
-                }
-            }
-        },
-        {
-            type: "input",
-            name: "intern_name",
-            message: "Please enter intern's name",
-            when: (answers) => {
-                if (answers.employee_type === "Intern") {
-                    return true;
-                }
-            }
-        },
-        {
-            type: "input",
-            name: "intern_id",
-            message: "Please enter intern's id",
-            when: (answers) => {
-                if (answers.employee_type === "Intern") {
-                    return true;
-                }
-            }
-        },
-        {
-            type: "input",
-            name: "intern_email",
-            message: "Please enter intern's email",
-            when: (answers) => {
-                if (answers.employee_type === "Intern") {
-                    return true;
-                }
-            }
-        },
-        {
-            type: "input",
-            name: "intern_school",
-            message: "Please enter intern's school",
-            when: (answers) => {
-                if (answers.employee_type === "Intern") {
-                    return true;
-                }
-            }
-        },
-        {
-            type: 'confirm',
-            name: 'continue',
-            message: 'Would you like to enter another employee?',
-        }
-    ]);
+// // Initialize a new Game object
+const employee = new Employee();
+
+// // Start playing
+employee.setTeam();
+
+// const inquirer = require('inquirer');
+// const fs = require('fs');
+
+// inquirer
+//     .prompt([
+//         {
+//             type: 'input',
+//             message: "What is your team manager's name?",
+//             name: 'manager_name',
+//         },
+//         {
+//             type: 'input',
+//             message: 'Please enter your employee ID?',
+//             name: 'manager_id',
+//         },
+//         {
+//             type: 'input',
+//             message: 'Please enter your email address',
+//             name: 'manager_email',
+//         },
+//         {
+//             type: 'input',
+//             message: 'Please enter your office number',
+//             name: 'manager_office_number',
+//         },
+//         {
+//             type: 'list',
+//             message: 'Add an engineer or intern to your page?',
+//             choices: ['Engineer', 'Intern'],
+//             name: 'employee_type',
+//         },
+//         {
+//             type: "input",
+//             name: "engineer_name",
+//             message: "Please enter engineer's name",
+//             when: (answers) => {
+//                 if (answers.employee_type === "Engineer") {
+//                     return true;
+//                 }
+//             }
+//         },
+//         {
+//             type: "input",
+//             name: "engineer_id",
+//             message: "Please enter engineer's id",
+//             when: (answers) => {
+//                 if (answers.employee_type === "Engineer") {
+//                     return true;
+//                 }
+//             }
+//         },
+//         {
+//             type: "input",
+//             name: "engineer_email",
+//             message: "Please enter engineer's email",
+//             when: (answers) => {
+//                 if (answers.employee_type === "Engineer") {
+//                     return true;
+//                 }
+//             }
+//         }, {
+//             type: "input",
+//             name: "engineer_github",
+//             message: "Please enter engineer's github",
+//             when: (answers) => {
+//                 if (answers.employee_type === "Engineer") {
+//                     return true;
+//                 }
+//             }
+//         },
+//         {
+//             type: "input",
+//             name: "intern_name",
+//             message: "Please enter intern's name",
+//             when: (answers) => {
+//                 if (answers.employee_type === "Intern") {
+//                     return true;
+//                 }
+//             }
+//         },
+//         {
+//             type: "input",
+//             name: "intern_id",
+//             message: "Please enter intern's id",
+//             when: (answers) => {
+//                 if (answers.employee_type === "Intern") {
+//                     return true;
+//                 }
+//             }
+//         },
+//         {
+//             type: "input",
+//             name: "intern_email",
+//             message: "Please enter intern's email",
+//             when: (answers) => {
+//                 if (answers.employee_type === "Intern") {
+//                     return true;
+//                 }
+//             }
+//         },
+//         {
+//             type: "input",
+//             name: "intern_school",
+//             message: "Please enter intern's school",
+//             when: (answers) => {
+//                 if (answers.employee_type === "Intern") {
+//                     return true;
+//                 }
+//             }
+//         },
+//         {
+//             type: 'confirm',
+//             name: 'continue',
+//             message: 'Would you like to enter another employee?',
+//         }
+//     ]);
 
 
 
